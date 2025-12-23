bab050630d3a28dc27f76f239406f03c7e65f2a3
atef.sowar <Atef.Sowar@xyrisdigital.com>
2025-12-23 10:25:32 +0200

    docs: add comments to describe stips, stip by stip

--- app/Http/Middleware/IdempotencyMiddleware.php
+++ app/Http/Middleware/IdempotencyMiddleware.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Cache;
 class IdempotencyMiddleware
 {
     /**
+     * This is a simple example of idempotency middleware. You can improve it by adding more features based on your case
      * Handle an incoming request.
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
@@ -20,34 +21,38 @@ class IdempotencyMiddleware
 
 
 
-        // 1- get idempotency key from header
+        ##################### 1- get idempotency key from header #####################
         $idempotencyKey = $request->header('X-Idempotency-Key');   
 
-        // 2- if no key-> proced with controler logic.
+        ##################### 2- if no key-> proced with controler logic. #####################
         if(!$idempotencyKey)
         {
             return $next($request);
         }
 
-       
-        // 3 - if key exists in cache -> return cached response.
-        if(Cache::has("idempotency_{$idempotencyKey}"))
-        {
-            $cacheResponse = Cache::get("idempotency_{$idempotencyKey}");
-            return response()->json($cacheResponse->original ? $cacheResponse->original : $cacheResponse ,200 , ['x-cache' => 'HIT-IDENTICAL']);
-        }
+        ##################### 3- if key not exists in cache -> proced with controler logic. #####################
 
-        // 4 - if key not exists in cache -> proced with controler logic.
-
+        // 3.1 - save response in variable(response) to check if it is successful.
         $response = $next($request);
         
+        // 3.2 - if response is successful -> save response in cache and named it idempotency_$idempotencyKey and add 10 hours to it.
         if($response->isSuccessful())
         {
             Cache::put("idempotency_{$idempotencyKey}", $response, now()->addHours(10));
         }
 
+       
+        ##################### 4- if key exists in cache -> return cached response. #####################
+        if(Cache::has("idempotency_{$idempotencyKey}"))
+        {
+            // 4.1 - get cached response.
+            $cacheResponse = Cache::get("idempotency_{$idempotencyKey}");
 
-        // 5 - return response.
+            // 4.2 - return original response from cached and add x-cache header to notify client that response is cached.
+            return response()->json($cacheResponse->original ? $cacheResponse->original : $cacheResponse ,200 , ['x-cache' => 'HIT-IDENTICAL']);
+        }
+
+        ##################### 5- return response. #####################
         return $response;
     }
 }
